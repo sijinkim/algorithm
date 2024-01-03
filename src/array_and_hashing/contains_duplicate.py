110e1bec6b053feac160ec9a81c7d5d4aedf004d
sijinkim <tlwlsdi0306@gmail.com>
2024-01-03 12:24:38 +0900

    [add] duplicate_check_more_faster 함수 추가
    - set 구조 적용하여 duplicate array에서 runtime 줄임
    - len(self.nums) > len(set(self.nums))의경우 set 생성에 nums 길이 디펜던시 생겨 항상 worst case 발생(O(n))
    - set containment/add (O(1)) 만 사용하여 duplicate elements의 경우 return True 종료
    - 길이 5000짜리 random.randint() array로 테스트 결과
        duplicate_check: 0.9917ms
        duplicate_check_faster: 0.2035ms
        duplicate_check_more_faster: 0.0536ms

--- src/array_and_hashing/contains_duplicate.py
+++ src/array_and_hashing/contains_duplicate.py
@@ -34,7 +34,25 @@ class ContainsDuplicateSolution(BaseModel):
 
     @timer
     def duplicate_check_more_faster(self) -> bool:
-        return len(self.nums) > len(set(self.nums))
+        """
+        #1. sort -> set의 bad case인 BigO(n) 보다 작은 sorting 알고리즘 적용 필요
+        #2. sorting 과정에서 동일한 item 있으면 return True
+        #3. sorting이 완료되면 return False
+
+        sorting 알고리즘 Worst(BigO) 확인 결과, Heap Sort/Merge Sort O(n log(n))
+        set(...) n이 큰 worst case에서 O(n)보다 결과 나쁨
+
+        때문에 #1,2,3 대신 set.add (O(1)) 를 활용한다.
+            1. n = 0 짜리 set 생성
+            2. nums iter 돌면서 set에 element가 있는지 확인
+            3. 없으면 set.add / 있으면 return True 종료
+        """
+        check_set: set[int] = set()
+        for n in self.nums:
+            if n in check_set:
+                return True
+            check_set.add(n)
+        return False
 
     def __repr__(self) -> str:
         return f"ContainsDuplicateSolution object: {id(self)}"
