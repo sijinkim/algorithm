3fe26015f8964bfa18bf099e4ee679c7e579890f
sijinkim <tlwlsdi0306@gmail.com>
2024-01-03 11:37:45 +0900

    [add] duplicate_check_faster 함수 추가
    - Counter 특성상 반드시 전체 리스트 확인 필요.
    - 전체 리스트를 확인하는 대신, 중복되는 elements가 발견될 때 True return 함수 추가
    - 기존 함수 대비 runtime 200ms 개선
    - 그러나 bad case에 대하여 기존 함수와 동일

--- src/array_and_hashing/contains_duplicate.py
+++ src/array_and_hashing/contains_duplicate.py
@@ -20,5 +20,21 @@ class ContainsDuplicateSolution(BaseModel):
             return True
         return False
 
+    @timer
+    def duplicate_check_faster(self) -> bool:
+        count: dict[int, int] = {}
+        for n in self.nums:
+            try:
+                count[n] += 1
+            except KeyError:
+                count[n] = 1
+            else:
+                return True
+        return False
+
+    @timer
+    def duplicate_check_more_faster(self) -> bool:
+        return len(self.nums) > len(set(self.nums))
+
     def __repr__(self) -> str:
         return f"ContainsDuplicateSolution object: {id(self)}"
--- tests/array_and_hashing/test_contains_duplicate.py
+++ tests/array_and_hashing/test_contains_duplicate.py
@@ -13,3 +13,31 @@ def test_can_check_with_duplicate_numbers():
     solution = ContainsDuplicateSolution(nums=nums)
 
     assert solution.duplicate_check() == True
+
+
+def test_can_check_faster_without_duplicate_numbers():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    solution = ContainsDuplicateSolution(nums=nums)
+
+    assert solution.duplicate_check_faster() == False
+
+
+def test_can_check_faster_with_duplicate_numbers():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+    solution = ContainsDuplicateSolution(nums=nums)
+
+    assert solution.duplicate_check_faster() == True
+
+
+def test_can_check_more_faster_without_duplicate_numbers():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    solution = ContainsDuplicateSolution(nums=nums)
+
+    assert solution.duplicate_check_more_faster() == False
+
+
+def test_can_check_more_faster_with_duplicate_numbers():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+    solution = ContainsDuplicateSolution(nums=nums)
+
+    assert solution.duplicate_check_more_faster() == True
