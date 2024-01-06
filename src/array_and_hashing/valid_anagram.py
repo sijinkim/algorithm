bd344ff4e96ee268621d536109312afd6b1483dd
sijinkim <tlwlsdi0306@gmail.com>
2024-01-06 10:31:27 +0900

    [add] valid anagram checker 기능 추가
    - BaseModel type check를 위한 validator 추가. 문제에 제시된 환경 사전 반영
    - is_anagram(): Counter 활용 s elements 확인에 O(n), 이후 t 루프 돌면서 early stop할 수 있도록 작성(s key에 t elements 없는 경우/ 이미 s 에서 해당 elements를 다 체크한 경우)
    - is_anagram_faster(): s counting O(n) + t countintg O(n) -> o(n) . Counter 활용 적합 문제로 판단됨

--- src/array_and_hashing/valid_anagram.py
+++ src/array_and_hashing/valid_anagram.py
@@ -3,11 +3,42 @@
 
 242.Valid Anagram (https://leetcode.com/problems/valid-anagram)
 """
-from pydantic import BaseModel
+from collections import Counter
+
+from pydantic import BaseModel, field_validator
+
+from src import timer
+
 
 class ValidAnagramSolution(BaseModel):
     s: str
     t: str
 
-    def isAnagram(self) -> bool:
-        ...
+    @field_validator("s", "t")
+    def check_length(cls, v: str) -> str:
+        if len(v) < 1 or len(v) > 5 * 10**4:
+            raise ValueError("1 <= s.length, t.length <= 5 * 10^4")
+        return v
+
+    @timer
+    def is_anagram(self) -> bool:
+        if not len(self.s) == len(self.t):
+            return False
+
+        s_counts = Counter(self.s)
+        for i in self.t:
+            if i not in s_counts:
+                return False
+
+            if s_counts[i] == 0:
+                return False
+
+            s_counts[i] -= 1
+
+        return True
+
+    @timer
+    def is_anagram_faster(self) -> bool:
+        if not len(self.s) == len(self.t):
+            return False
+        return Counter(self.s) == Counter(self.t)
--- tests/array_and_hashing/test_valid_anagram.py
+++ tests/array_and_hashing/test_valid_anagram.py
@@ -1,13 +1,39 @@
+import pytest
+
 from src.array_and_hashing import ValidAnagramSolution
+
 
 def test_true_case():
     s = "anagram"
-    t = "nagram"
+    t = "nagaram"
 
-    assert ValidAnagramSolution(s, t).isAnagram() == True
+    assert ValidAnagramSolution(s=s, t=t).is_anagram() == True
+
 
 def test_false_case():
-    s = "anagram"
-    t = "antaram"
+    s = "aacc"
+    t = "ccac"
 
-    assert ValidAnagramSolution(s, t).isAnagram() == False
+    assert ValidAnagramSolution(s=s, t=t).is_anagram() == False
+
+
+def test_length_checker():
+    s = ""
+    t = "ksk"
+
+    with pytest.raises(ValueError) as excinfo:
+        ValidAnagramSolution(s=s, t=t)
+
+
+def test_true_case_faster():
+    s = "anagram"
+    t = "nagaram"
+
+    assert ValidAnagramSolution(s=s, t=t).is_anagram_faster() == True
+
+
+def test_false_case_faster():
+    s = "aacc"
+    t = "ccac"
+
+    assert ValidAnagramSolution(s=s, t=t).is_anagram_faster() == False
