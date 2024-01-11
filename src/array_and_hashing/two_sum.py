4aeb022da07d8b8b030658c539db75826adc75c9
sijinkim <tlwlsdi0306@gmail.com>
2024-01-11 17:22:15 +0900

    [add] base function

--- src/array_and_hashing/two_sum.py
+++ src/array_and_hashing/two_sum.py
@@ -12,7 +12,17 @@ class TwoSumSolution(BaseModel):
 
     def two_sum(self) -> list[int]:
         """
-        1. nums item 하나씩 돌면서 다른 item과 더해 target과 동일한지 확인
-            O(n^2)
+        1. nums item 하나씩 돌면서 다른 item과 더해 target과 동일한지 확인 =>  O(n^2)
         """
-        ...
+        idx: int = 0
+        result: list[int] = []
+        for idx, n in enumerate(self.nums):
+            count = 1
+            while idx + count < len(self.nums):
+                if n + self.nums[idx + count] == self.target:
+                    result = [idx, idx + count]
+                    break
+                count += 1
+            if len(result) == 2:
+                break
+        return result
