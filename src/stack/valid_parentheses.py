7bcdc02851b9d768a943b8a7c079eb94adcf5436
sijinkim <tlwlsdi0306@gmail.com>
2024-01-26 22:48:52 +0900

    [add] valid parentheses base algorithm O(N)

--- src/stack/valid_parentheses.py
+++ src/stack/valid_parentheses.py
@@ -5,8 +5,33 @@
 """
 from pydantic import BaseModel
 
+
 class ValidParenthesesSolution(BaseModel):
     s: str
 
     def is_valid(self) -> bool:
-        ...
+        """
+        0. s 순회하면서 괄호 타입별로, open/close 쌍 맞는지 체크
+
+        O(N)
+        """
+        if not len(self.s) % 2 == 0:
+            return False
+
+        result: list[str] = []
+
+        for item in self.s:
+            if item in "([{":
+                result.append(item)
+
+            if item in "}])" and len(result) > 0:
+                if item == "}" and result[-1] == "{":
+                    result.pop()
+                elif item == "]" and result[-1] == "[":
+                    result.pop()
+                elif item == ")" and result[-1] == "(":
+                    result.pop()
+                else:
+                    return False
+
+        return len(result) == 0
---
+++ tests/stack/test_valid_parentheses.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.stack import ValidParenthesesSolution
+
+
+@pytest.fixture
+def inputs() -> list[str]:
+    return ["()", "()[]{}", "()]", "{{}}]", "()[", "{[{()}}"]
+
+
+def test_can_check_valid(inputs):
+    assert ValidParenthesesSolution(s=inputs[0]).is_valid() == True
+    assert ValidParenthesesSolution(s=inputs[1]).is_valid() == True
+
+
+def test_can_check_not_opened(inputs):
+    assert ValidParenthesesSolution(s=inputs[2]).is_valid() == False
+    assert ValidParenthesesSolution(s=inputs[3]).is_valid() == False
+
+
+def test_can_check_not_closed(inputs):
+    assert ValidParenthesesSolution(s=inputs[4]).is_valid() == False
+    assert ValidParenthesesSolution(s=inputs[5]).is_valid() == False
