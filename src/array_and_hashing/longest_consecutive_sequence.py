8e2a96bab01bbb69a68965ed942b2b0705383a0b
sijinkim <tlwlsdi0306@gmail.com>
2024-01-26 13:03:10 +0900

    [add] sort, set 활용 O(NlogN) + O(N) method

--- src/array_and_hashing/longest_consecutive_sequence.py
+++ src/array_and_hashing/longest_consecutive_sequence.py
@@ -13,4 +13,37 @@ class LongestConsecutiveSequenceSolution(BaseModel):
 
     @timer
     def longest_consecutive(self) -> int:
-        ...
+        """
+        0. O(N) time complexity로, 연속된 integer sequence max 길이 찾기
+            -> base method: sorting 활용
+        1. nums set sorting
+        3. max_length=0, sorted nums 순회하면서
+            3.1 다음 value와 현재 value가 1 차이 나는 경우 curr_length count up.
+            3.2 다음 value와 현재 value가 1 차이 아닌 경우, curr_length 와 max_length 비교
+            3.3 더 큰 값을 max_length로
+
+        O(NlogN) + O(N)
+        0.0264 ms
+        """
+        if len(self.nums) == 0:
+            return 0
+
+        sorted_nums_set: list[int] = sorted(list(set(self.nums)))  # O(NlogN) + O(N)
+
+        max_length: int = 0
+        curr_length: int = 1
+
+        len_nums = len(sorted_nums_set)
+        if len_nums == 1:
+            return 1
+
+        for i in range(len_nums - 1):  # O(N)
+            if abs(sorted_nums_set[i] - sorted_nums_set[i + 1]) == 1:
+                curr_length += 1
+
+            else:
+                curr_length = 1
+
+            max_length = max(max_length, curr_length)
+
+        return max_length
--- tests/array_and_hashing/test_longest_consecutive_sequence.py
+++ tests/array_and_hashing/test_longest_consecutive_sequence.py
@@ -1,13 +1,39 @@
+import pytest
+
 from src.array_and_hashing import LongestConsecutiveSequenceSolution
 
 
-def test_can_check_consecutive_sequence():
-    ...
+@pytest.fixture
+def nums():
+    return [
+        [100, 4, 200, 1, 3, 2],
+        [11, 22, 33, 44, 55],
+        [5, 3, 2, 1, 4, 1, 3, 2, 4, 5],
+        [],
+        [1, 2, 3, 3, 3, 3, 4, 5, 6, 7],
+        [0],
+    ]
 
 
-def test_can_check_no_sequence():
-    ...
+def test_can_check_consecutive_sequence(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[0]).longest_consecutive() == 4
 
 
-def test_can_check_duplicate_sequence():
-    ...
+def test_can_check_no_sequence(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[1]).longest_consecutive() == 1
+
+
+def test_can_check_duplicate_sequence(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[2]).longest_consecutive() == 5
+
+
+def test_can_check_empty_nums(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[3]).longest_consecutive() == 0
+
+
+def test_can_restart_counting(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[4]).longest_consecutive() == 7
+
+
+def test_can_check_single_element(nums):
+    assert LongestConsecutiveSequenceSolution(nums=nums[5]).longest_consecutive() == 1
