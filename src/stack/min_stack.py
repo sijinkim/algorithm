7ea6c48f0aedd6b364322f8f155aa2c9aad16421
sijinkim <tlwlsdi0306@gmail.com>
2024-01-28 11:14:38 +0900

    [fix] length 0 인 경우 처리, min() 처리에 유용한 float(inf) 사용

--- src/stack/min_stack.py
+++ src/stack/min_stack.py
@@ -3,31 +3,25 @@
 
 155.Min Stack (https://leetcode.com/problems/min-stack/)
 """
-from typing import cast
 
 
 class MinStack:
     """
     self.min_value와 각 노드에 min 값 담기를 같이 할 이유가 있을까?
     """
+
     def __init__(self) -> None:
-        self.stack: list[dict[int | None, int | None]] = [{None: None}]
-        self.min_value: int | None = None
+        self.stack: list[tuple[int, float]] = []
 
     def push(self, val: int) -> None:
-        if not self.min_value is None:
-            self.min_value = min(val, self.min_value)
-        else:
-            self.min_value = val
-
-        self.stack.append({val: self.min_value})
+        min_value = self.stack[-1][1] if len(self.stack) > 0 else float("inf")
+        self.stack.append((val, min(val, min_value)))
 
     def pop(self) -> None:
         self.stack.pop()
-        self.min_value = list(self.stack[-1].items())[0][1]
 
-    def top(self) -> int:
-        return cast(int, list(self.stack[-1].items())[0][0])
+    def top(self) -> float:
+        return self.stack[-1][0]
 
-    def get_min(self) -> int:
-        return self.min_value
+    def get_min(self) -> float:
+        return self.stack[-1][1]
