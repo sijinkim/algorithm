7d4aaedddf90c1b36954a0f7cde928cfbc71d317
sijinkim <tlwlsdi0306@gmail.com>
2024-01-28 10:42:34 +0900

    [add] min_value 값 저장/업데이트 활용

--- src/stack/min_stack.py
+++ src/stack/min_stack.py
@@ -3,25 +3,31 @@
 
 155.Min Stack (https://leetcode.com/problems/min-stack/)
 """
-from pydantic import BaseModel, Field
+from typing import cast
 
 
-class MinStack(BaseModel):
-    stack: list[dict[int, int]] = Field(default=[])
-    # { 현재 노드 값: 현재 노드 포함, 지금까지 push된 값들 중 가장 작은 값}
-    min_value: int = Field(default=0)
+class MinStack:
+    """
+    self.min_value와 각 노드에 min 값 담기를 같이 할 이유가 있을까?
+    """
+    def __init__(self) -> None:
+        self.stack: list[dict[int | None, int | None]] = [{None: None}]
+        self.min_value: int | None = None
 
     def push(self, val: int) -> None:
-        if len(self.stack) == 0:
+        if not self.min_value is None:
+            self.min_value = min(val, self.min_value)
+        else:
             self.min_value = val
-        self.min_value = min(val, self.min_value)
+
         self.stack.append({val: self.min_value})
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_value = list(self.stack[-1].items())[0][1]
 
     def top(self) -> int:
-        return list(self.stack[-1].items())[0][0]
+        return cast(int, list(self.stack[-1].items())[0][0])
 
     def get_min(self) -> int:
-        return list(self.stack[-1].items())[0][1]
+        return self.min_value
