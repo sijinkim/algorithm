572cb8e2c7038564c772c29677027e532a81ab70
sijinkim <tlwlsdi0306@gmail.com>
2024-02-23 14:24:50 +0900

    [add] ORDER BY 조건 추가

--- src/sql/practice4.sql
+++ src/sql/practice4.sql
@@ -15,3 +15,13 @@ SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION != 'Aged'
 ORDER BY ANIMAL_ID
+
+-- 2024-02-23: https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- ANIMAL_INS: 동물 보호소 동물 정보 테이블
+-- 동물 보호소에 들어온 모든 동물의 아이디와 이름, 보호 시작일을 이름 순으로 조회
+-- 추가 조건) 이름이 같은 동물 중에서는 보호를 나중에 시작한 동물을 먼저 보여줘야 함
+-- 이름이 같다면, DATETIME이 큰 것 우선 정렬
+
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC
