f6e82e4051b84578dee628f11672ea9ad4606f12
sijinkim <tlwlsdi0306@gmail.com>
2024-03-11 23:27:55 +0900

    [add] min, max pointer 사용하여 O(N)으로 합쳐서 target되는 value 찾기

--- src/two_pointers/two_sum_sorted.py
+++ src/two_pointers/two_sum_sorted.py
@@ -1,7 +1,52 @@
 """
 2024-03-11
-167. Two Sum II - Input Array Is Sorted (https://leetcode.com/problems/two-sum-ii-input-array-is-sorted)
+167. Two Sum II - Input Array Is Sorted 
+(https://leetcode.com/problems/two-sum-ii-input-array-is-sorted)
 """
+
+
 class TwoSumSolution:
     def solution(self, numbers: list[int], target: int) -> list[int]:
-        ...
+        """
+        조건
+        0. numbers: sorted list (non-decreasing order)
+        1. 1 <= index1 < index2 <= numbers.length
+        2. 2 <= numbers.length <= 3 * 10_000
+        3. exactly one solution
+
+        O(N*N)
+        """
+        answer: list[int] = []
+        idx = 0
+        while not answer:
+            num1 = numbers[idx]
+            num2 = target - num1
+
+            for num2_idx in range(idx + 1, len(numbers)):
+                if num2 == numbers[num2_idx]:
+                    answer = [idx + 1, num2_idx + 1]
+            idx += 1
+
+        return answer
+
+    def solution_faster(self, numbers: list[int], target: int) -> list[int]:
+        """
+        O(N)
+        """
+        min_idx = 0
+        max_idx = len(numbers) - 1
+
+        answer: list[int] = []
+        while min_idx < max_idx:
+            min_max_sum = numbers[min_idx] + numbers[max_idx]
+            if min_max_sum > target:
+                max_idx -= 1
+
+            elif min_max_sum < target:
+                min_idx += 1
+
+            else:
+                answer = [min_idx + 1, max_idx + 1]
+                break
+
+        return answer
--- tests/two_pointers/test_two_sum_sorted.py
+++ tests/two_pointers/test_two_sum_sorted.py
@@ -1,4 +1,20 @@
 from src.two_pointers.two_sum_sorted import TwoSumSolution
 
+
 def test_can_get_correct_answer():
-    assert TwoSumSolution().solution(numbers = [2,7,11,15], target = 9) == [1,2]
+    assert TwoSumSolution().solution(numbers=[2, 7, 11, 15], target=9) == [1, 2]
+
+
+def test_can_get_target_zero():
+    assert TwoSumSolution().solution(numbers=[-1, 0, 0, 3, 5], target=0) == [2, 3]
+
+
+def test_can_get_correct_answer_faster():
+    assert TwoSumSolution().solution_faster(numbers=[2, 7, 11, 15], target=9) == [1, 2]
+
+
+def test_can_get_target_zero_faster():
+    assert TwoSumSolution().solution_faster(numbers=[-1, 0, 0, 3, 5], target=0) == [
+        2,
+        3,
+    ]
